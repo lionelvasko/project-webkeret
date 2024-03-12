2b2466e387eca81150bc4779f364a6a2a030e941
Lionel Vaskó <119498065+lionelvasko@users.noreply.github.com>
2024-03-12 11:04:55 +0100

    firebase-stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -14,6 +14,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -28,14 +29,15 @@ import { getStorage, provideStorage } from '@angular/fire/storage';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    provideFirebaseApp(() => initializeApp({"projectId":"cinema-web-mobil-4d142","appId":"1:736206924636:web:1c8ff2d7e7a0c8007eef99","storageBucket":"cinema-web-mobil-4d142.appspot.com","apiKey":"AIzaSyBTiD6vcDr-KiiuCzdO_0eECFjJAuKXHzY","authDomain":"cinema-web-mobil-4d142.firebaseapp.com","messagingSenderId":"736206924636","measurementId":"G-DNT284G99N"})),
+    provideFirebaseApp(() => initializeApp(environment.firebase),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
   ],
   providers: [
     provideClientHydration(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    firebase:{
+        apiKey: "AIzaSyBTiD6vcDr-KiiuCzdO_0eECFjJAuKXHzY",
+        authDomain: "cinema-web-mobil-4d142.firebaseapp.com",
+        projectId: "cinema-web-mobil-4d142",
+        storageBucket: "cinema-web-mobil-4d142.appspot.com",
+        messagingSenderId: "736206924636",
+        appId: "1:736206924636:web:1c8ff2d7e7a0c8007eef99",
+        measurementId: "G-DNT284G99N"
+      },
+        production: false,
+        hostUrl: 'http://localhost:4200'
+};
