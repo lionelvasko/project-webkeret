f21c3935b50b32a689d0dc2ae36e0ae38675816c
Lionel Vaskó <119498065+lionelvasko@users.noreply.github.com>
2024-03-15 15:37:17 +0100

    profile/v2

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
 import { StorageService } from '../../shared/services/storage.service';
 import { getAuth } from "firebase/auth";
+import { BehaviorSubject } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -9,34 +11,52 @@ import { getAuth } from "firebase/auth";
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit, OnDestroy{
 
 
-  name = new FormControl('');
-  email = new FormControl('');
-  phone = new FormControl('');
-  address = new FormControl('');
+  name = new BehaviorSubject<string>('');
+  email = new BehaviorSubject<string>('');
+  phone = new BehaviorSubject<string>('');
+  address = new BehaviorSubject<string>('');
+
   currentuser;
   auth = getAuth();
 
   constructor(private storageService: StorageService) {
     this.currentuser = this.auth.currentUser;
+  }
+  ngOnInit(): void {
+    this.currentuser = this.auth.currentUser;
     if (this.currentuser?.uid) {
-      this.storageService.getUserData(this.currentuser.uid).then((data: any) => {
-        if (data) {
-          this.name.setValue(data.name);
-          this.email.setValue(data.email);
-          this.phone.setValue(data.phone);
-          this.address.setValue(data.address);
-        }
-      });
-    }
+    this.storageService.getUserData(this.currentuser.uid).then((data: any) => {
+      if (data) {
+        this.name.next(data.name);
+        this.email.next(data.email);
+        this.phone.next(data.phone);
+        this.address.next(data.address);
+      }
+    });
+  }
+  }
+  ngOnDestroy(): void {
+    this.name.complete();
+    this.email.complete();
+    this.phone.complete();
+    this.address.complete();
   }
 
 
   async change(event: Event) {
     event.preventDefault();
-
+    if (this.currentuser?.uid) {
+      this.storageService.updateCurrentUser(
+        this.currentuser.uid,
+        this.name.value,
+        this.email.value,
+        this.phone.value,
+        this.address.value
+      );
+    }
   }
 
 }
