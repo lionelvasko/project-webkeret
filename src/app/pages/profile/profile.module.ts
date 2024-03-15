cd501ed04946a24c1bc2997672036403fdbb21ec
Lionel Vaskó <119498065+lionelvasko@users.noreply.github.com>
2024-03-15 10:59:52 +0100

    profilev-v1.0

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import {MatTabsModule} from '@angular/material/tabs';
-import { ProfileThingComponent } from './profile-thing/profile-thing.component';
 import { TicketsComponent } from './tickets/tickets.component';
 
 
 @NgModule({
   declarations: [
     ProfileComponent,
-    ProfileThingComponent,
     TicketsComponent
   ],
   imports: [
--- src/app/shared/services/storage.service.ts
+++ src/app/shared/services/storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, addDoc, collection, getDocs, query } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, getDocs, query, where } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +14,27 @@ export class StorageService {
       password: password
     });
     console.log("Document written with ID: ", docRef.id);
+  }
+
+  async getUserData(email: string) {
+    const q = query(collection(this.firestore, 'users'), where("email", "==", email));
+    const querySnapshot = await getDocs(q);
+    let userData = null;
+    querySnapshot.forEach((doc) => {
+        userData = { id: doc.id, ...doc.data() };
+    });
+    return userData;
+}
+
+async updateCurrentUser(name: string, email: string, password: string, phone: string, address: string) {
+  const existingData = await this.getUserData(email) as unknown as { name: string, email: string, password: string, phone: string, address: string };
+  const docRef = await addDoc(collection(this.firestore, 'users'), {
+    name: name ? name : (existingData ? existingData.name : ''),
+    email: email ? email : (existingData ? existingData.email : ''),
+    password: password ? password : (existingData ? existingData.password : ''),
+    phone: phone ? phone : (existingData ? existingData.phone : ''),
+    address: address ? address : (existingData ? existingData.address : '')
+  });
+  console.log("Document written with ID: ", docRef.id);
 }
 }
