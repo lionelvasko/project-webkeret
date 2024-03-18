ba76ba8ffd8d3640757ca5923622348a30d3bf25
Lionel Vaskó <119498065+lionelvasko@users.noreply.github.com>
2024-03-18 19:46:44 +0100

    profile auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './shared/services/auth.guard';
 
 const routes: Routes = [
   {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
   {path: 'login', loadChildren:() => import('./pages/login/login.module').then(m => m.LoginModule)},
   {path: 'register', loadChildren:() => import('./pages/register/register.module').then(m => m.RegisterModule)},
   {path: 'shows', loadChildren:() => import('./pages/shows/shows.module').then(m => m.ShowsModule)},
-  {path: 'profile', loadChildren:() => import('./pages/profile/profile.module').then(m => m.ProfileModule)},
+  {path: 'profile',canActivate: [AuthGuard], loadChildren:() => import('./pages/profile/profile.module').then(m => m.ProfileModule)},
   {path: 'not-found', loadChildren:() => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule)},
   {path: 'login/forgot-pswd', loadChildren: () => import('./pages/login/forgot-pswd/forgot-pswd.module').then(m => m.ForgotPswdModule) },
   {path: 'logout', loadChildren: () => import('./pages/logout/logout.module').then(m => m.LogoutModule)},
